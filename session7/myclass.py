543f456a015f43cafcdd8ea9b1259b489d1ff531
Juan Garrido <juan.garrido.arcos95@gmail.com>
2016-10-22 17:30:51 +0200

    Commit 3 min

--- session7/myclass.py
+++ session7/myclass.py
@@ -1,3 +1,8 @@
 class MyClass:
     def __init__(self, fila, columna):
         self.array = []
+        for i in range(fila):
+            self.temp = []
+            for j in range(columna):
+                self.temp.append(0)
+            self.array.append(self.temp)
---
+++ session7/test.py
@@ -0,0 +1,6 @@
+import myclass
+
+def test_cells_created():
+    t1 = myclass.MyClass(2,2)
+    t1.array[0, 0] = 1
+    t1.array[1, 0] = 1
