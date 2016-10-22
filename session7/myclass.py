e7bbc2a219011a903a258de8a43fadf548157a99
Juan Garrido <juan.garrido.arcos95@gmail.com>
2016-10-22 17:59:13 +0200

    Commit 3 min

--- session7/myclass.py
+++ session7/myclass.py
@@ -1,3 +1,4 @@
+import math
 class MyClass:
     def __init__(self, fila, columna):
         self.array = []
@@ -9,3 +10,18 @@ class MyClass:
 
     def count_cells(self):
        return  str(self.array).count('1')
+
+    def get_neighbours(self, x, y):
+        def distance(pos1x, pos1y, pos2x, pos2y):
+            if (abs(pos1x-pos2x) <= 1 and abs(pos1y-pos2y)<= 1):
+                return True
+            else:
+                return False
+
+        contador = 0
+        for i in range(len(self.array)):
+            for j in range(len(self.array[0])):
+                if distance(x,y, i,j):
+                    if self.array[i][j] == 1:
+                        contador+=1
+        return contador
--- session7/test.py
+++ session7/test.py
@@ -14,4 +14,9 @@ def test_count_cells():
 
     assert (t1.count_cells()== 2)
 
+def test_count_neighbours():
+    t1 = myclass.MyClass(2,2)
+    t1.array[0][0] = 1
+    t1.array[1][0] = 1
 
+    assert (t1.get_neighbours(0,1) == 2)
